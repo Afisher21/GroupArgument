3c752acf5a6ab7a50008a9bdae7def9a602b11a2
Austin Fisher (COSINE-ENS) <aufishe@microsoft.com>
2019-11-18 08:33:20 -0800

    Buggy implementation of next, but mostly functoinal

--- GameMasterWindow.py
+++ GameMasterWindow.py
@@ -3,6 +3,9 @@ import tkinter as tk
 from Classes import *
 from dotmap import DotMap
 
+g_RoundScore = 0
+g_obj = {}
+
 def ValidateWindowArgs(**kwargs):
     if 'master' not in kwargs or kwargs['master'] is None:
         raise Exception("'master' tk window not provided for adding objects to!")
@@ -44,18 +47,39 @@ def PopulatePlayers(**kwargs):
         # Add score box for this player
         tk.Label(kwargs['master'], text="0").grid(column=playerCol, row = kwargs['offsets'].PlayerScore)
 
+def PopulateAnswers(**kwargs):
+    for resp in range(len(kwargs['answers'])):
+        tk.Label(kwargs['master'], text=kwargs['answers'][resp][1]).grid(column=kwargs['offsets'].DisplayCount,row = kwargs['offsets'].AnswerBegin + resp)
+        tk.Label(kwargs['master'], text=kwargs['answers'][resp][0]).grid(column=kwargs['offsets'].DisplayAnswer,row = kwargs['offsets'].AnswerBegin + resp)
+
+def FindActivePlayer(**kwargs):
+    if len(kwargs['contestants']) is not 2:
+        raise Exception("FindActivePlayer is only valid when there are 2 players")
+    p1 = kwargs['master'].grid_slaves(column = kwargs['offsets'].PlayerStart)
+    for row in p1:
+        if isinstance(row, tk.ttk.Checkbutton):
+            if 'focus' in row.state():
+                return kwargs['offsets'].PlayerStart
+    p2 = kwargs['master'].grid_slaves(column = kwargs['offsets'].PlayerEnd)
+    for row in p2:
+        if isinstance(row, tk.ttk.Checkbutton):
+            if 'focus' in row.state():
+                return kwargs['offsets'].PlayerEnd
+    return 0
+
 def PopulateSubmitButtons(**kwargs):
     # Defines the Callback for when a "submit" button is pressed. Adds the points to the players score
     # Side note on useage - .grid_slaves returns a list of all matching values
     #  BugBug: Area of improvement would be to have a singular call to get row, and then parse through that
     def Submit(row):
         questionScoreBox = kwargs['master'].grid_slaves(row, kwargs['offsets'].DisplayCount)
-        questionScore = int(questionScoreBox[0]['text'])
+        questionScore = int(questionScoreBox[0]['text']) * int(kwargs['round'])
         answerProvided = False
         for col in range(len(kwargs['contestants'])):
             contestant = col + kwargs['offsets'].PlayerStart
             chkbox = kwargs['master'].grid_slaves(row, contestant)
-            if(chkbox[0].state()):
+            # Award points to whichever team(s) submitted the associated answer
+            if('selected' in chkbox[0].state()):
                 answerProvided = True
                 scorebox = kwargs['master'].grid_slaves(kwargs['offsets'].PlayerScore, contestant)
                 oldscore = int(scorebox[0]['text'])
@@ -73,6 +97,24 @@ def PopulateSubmitButtons(**kwargs):
             # Also Disable "Submit score" button
             submitBut = kwargs['master'].grid_slaves(row, kwargs['offsets'].SubmitButton)
             submitBut[0]['state'] = 'disabled'
+            # Keep track of score for 2 player game
+            if len(kwargs['contestants']) is 2:
+                global g_RoundScore
+                g_RoundScore += questionScore
+        # In 2 player games, only 1 player can score at a time.
+        # Disable other player to avoid accidental point giving
+        # BugBug: Could improve performance by only doing this once
+        if len(kwargs['contestants']) is 2:
+            inactiveplayer = None
+            active = FindActivePlayer(**kwargs)
+            if active is kwargs['offsets'].PlayerStart:
+                inactiveplayer = kwargs['master'].grid_slaves(column = kwargs['offsets'].PlayerEnd)
+            elif active is kwargs['offsets'].PlayerEnd:
+                inactiveplayer = kwargs['master'].grid_slaves(column = kwargs['offsets'].PlayerStart) 
+            if inactiveplayer is not None:
+                for row in inactiveplayer:
+                    if isinstance(row, tk.ttk.Checkbutton):
+                        row.state(['disabled'])
             
     # Add submit buttons. These are used when a contestant says a response that's already on the board
     for resp in range(len(kwargs['answers'])):
@@ -80,7 +122,58 @@ def PopulateSubmitButtons(**kwargs):
         but = tk.Button(kwargs['master'], text="Submit scoring")
         but['command'] = lambda rw=rw: Submit(rw)
         but.grid(column= kwargs['offsets'].SubmitButton, row=rw)
+        
 
+def PopulateGroupSwitch(**kwargs):
+    incorrectGuessText = 'Incorrect guess'
+    scoreSwapText = 'Round score stolen'
+    def DISABLE(button):
+        button["state"] = "disabled"
+        butGrid = kwargs['master'].grid_slaves(column = kwargs['offsets'].SubmitButton + 1)
+        active = False
+        for but in butGrid:
+            # only care about the 3 disabled buttons
+            if but['text'] == incorrectGuessText and but['state'] == 'normal':
+                active = True
+        if not active:
+            for but in butGrid:
+                if but['text'] == scoreSwapText:
+                    but['state'] = ['normal']
+        
+    def SWAP(button, score):
+        button["state"] = "disabled"
+        # Subtract round score from active player, add to inactive
+        prevTeam = FindActivePlayer(**kwargs)
+        newTeam = (kwargs['offsets'].PlayerStart if prevTeam is kwargs['offsets'].PlayerEnd else kwargs['offsets'].PlayerEnd)
+        prevTeamScore = kwargs['master'].grid_slaves(row = kwargs['offsets'].PlayerScore, column = prevTeam)[0]
+        prevTeamScore['text'] = int(prevTeamScore['text']) - g_RoundScore
+        newTeamScore = kwargs['master'].grid_slaves(row = kwargs['offsets'].PlayerScore, column = newTeam)[0]
+        newTeamScore['text'] = int(newTeamScore['text']) + g_RoundScore
+        # Swap which player is active
+        prevCol = kwargs['master'].grid_slaves(column = prevTeam)
+        for row in prevCol:
+            if isinstance(row, tk.ttk.Checkbutton):
+                if 'disabled' not in row.state():
+                    row.state(['disabled'])
+        newCol = kwargs['master'].grid_slaves(column = newTeam)
+        for iter in range(len(newCol)):
+            if isinstance(newCol[iter], tk.ttk.Checkbutton):
+                if 'disabled' in newCol[iter].state() and 'selected' not in prevCol[iter].state():
+                    newCol[iter].state(['!disabled'])
+        
+    # Traditional FF! This means we should keep track of incorrect answers
+    possibleIncorrects = 3
+    for i in range(possibleIncorrects):
+        but = tk.Button(kwargs['master'], text=incorrectGuessText)
+        but['command'] = lambda b=but: DISABLE(b)
+        but.grid(column = kwargs['offsets'].SubmitButton + 1, row = kwargs['offsets'].AnswerBegin + 2*i, rowspan=2)
+    
+    # Add "Score steal" button
+    but = tk.Button(kwargs['master'], text = scoreSwapText)
+    global g_RoundScore
+    but['command'] = lambda b=but, s=g_RoundScore: SWAP(b,s)
+    but['state'] = 'disabled'
+    but.grid(column = kwargs['offsets'].SubmitButton + 1, row = kwargs['offsets'].AnswerBegin + 2*possibleIncorrects, rowspan=2)
 
 def PopulateWindow(**kwargs):
     # Validate/provide shorthand for args
@@ -90,6 +183,7 @@ def PopulateWindow(**kwargs):
     contestants =   kwargs['contestants']
     answers     =   kwargs['answers']
     offsets     =   kwargs['offsets']
+    curRound    =   str(kwargs['round']) # Force str value
     
     # Populate the question that contestants are working with
     tk.Label(master, text=question, font=("Times",15)).grid(row=offsets.Question, columnspan=10)
@@ -97,28 +191,120 @@ def PopulateWindow(**kwargs):
     tk.Label(master, text='response').grid(row = offsets.Names, column = offsets.DisplayAnswer)
     
     # Populate the survey answers so that Game Master knows which ones are available
-    for resp in range(len(answers)):
-        tk.Label(master, text=answers[resp][1]).grid(column=offsets.DisplayCount,row = offsets.AnswerBegin + resp)
-        tk.Label(master, text=answers[resp][0]).grid(column=offsets.DisplayAnswer,row = offsets.AnswerBegin + resp)
-        
-     # Populate the playerbase
+    PopulateAnswers(**kwargs)
+    
+    # Populate the playerbase
     PopulatePlayers(**kwargs)
     
     # Populate Submit fields
     tk.Label(master, text="(SUBMIT)").grid(column= offsets.SubmitButton, row = offsets.Names)
     PopulateSubmitButtons(**kwargs)
     
+    # Populate "Incorrect guess" and "Steal round score" buttons
+    if len(contestants) is 2:
+        PopulateGroupSwitch(**kwargs)
+    
+    def NEXT_ROUND():
+        import re
+        global g_RoundScore
+        g_RoundScore = 0
+        PLAYER_COUNT = 1 + g_obj['offsets'].PlayerEnd - g_obj['offsets'].PlayerStart
+        if 'used' not in g_obj or g_obj['used'] is None:
+            g_obj['used'] = []
+        g_obj['used'] = g_obj['used'].append(g_obj['question'])
+        g_obj['question'] = None
+        g_obj['answers'] = None
+        # Store round multiplier & score( has to be done first due to lack of persistence in g_obj )
+        contestantScores = []
+        multLabel = g_obj['master'].grid_slaves(row = g_obj['offsets'].RoundMultiplier, column = 0)[0]
+        prevRound = int(re.findall('\d+', multLabel['text'])[0])
+        g_obj['round'] = str(prevRound + 1)
+        multLabel['text'] = "Round multiplier: " + g_obj['round']
+        for i in range(PLAYER_COUNT):
+            label = g_obj['master'].grid_slaves(row = g_obj['offsets'].PlayerScore, column = g_obj['offsets'].PlayerStart + i)[0]
+            contestantScores.append(label['text'])
+            label.destroy()
+        # Get new round content
+        newQ, newA = GetQuestionAndAnswers(**g_obj)
+        print('Round: ' + g_obj['round'] + ' question; "' + newQ + '"\n\tResponses: ' + str(newA))
+        # Clean up previous round answers and submit buttons
+        for i in range(1 + g_obj['offsets'].AnswerEnd - g_obj['offsets'].AnswerBegin):
+            aRow = g_obj['master'].grid_slaves( row = g_obj['offsets'].AnswerBegin + i)
+            for j in range(len(aRow)):
+                # grid_forget is the more common way to do this, but actually causes a memory leak as the object iself still exists
+                #  But now it exists where it can't be indexed( since it's off the grid) and we can't do anything with it as a result
+                if j >= g_obj['offsets'].PlayerStart and j <= g_obj['offsets'].SubmitButton:
+                    aRow[j].destroy()
+        # Update g_obj contents
+        g_obj['answers'] = newA
+        g_obj['offsets'].AnswerEnd = g_obj['offsets'].AnswerBegin + len(newA)
+        g_obj['question'] = newQ
+        nextBut = g_obj['master'].grid_slaves(row = g_obj['offsets'].PlayerScore + 1)[0]
+        g_obj['offsets'].PlayerScore = g_obj['offsets'].AnswerEnd + 1
+        g_obj['offsets'].RoundMultiplier = g_obj['offsets'].PlayerScore
+        
+        # Update static fields location & contents
+        questLabel = g_obj['master'].grid_slaves(row = g_obj['offsets'].Question)[0]
+        questLabel['text'] = newQ
+        nextBut.grid(columnspan = 10, row = offsets.PlayerScore + 1)
+        multLabel.grid(row = g_obj['offsets'].RoundMultiplier, column = 0)
+        
+        PopulateAnswers(**g_obj)
+        PopulatePlayers(**g_obj)
+        PopulateSubmitButtons(**g_obj)
+        # restore value of scores
+        for i in range(PLAYER_COUNT):
+            label = g_obj['master'].grid_slaves(row = g_obj['offsets'].PlayerScore, column = g_obj['offsets'].PlayerStart + i)[0]
+            label['text'] = contestantScores[i]
+        
+    roundLabel = tk.Label(master, text= 'Round multiplier: ' + curRound)
+    roundLabel.grid(row = offsets.RoundMultiplier, column = 0)
+    but = tk.Button(master, text = 'Next Round')
+    but['command'] = lambda: NEXT_ROUND()
+    but.grid(columnspan=10, row = offsets.PlayerScore + 1)
+    g_obj = kwargs
+
+def GetQuestionAndAnswers(**kwargs):
+    questionParam = 'question'
+    usedQuestionsParam = 'used'
+    answerParam = 'answers'
+    randomParam = 'random'
+    databaseParam = 'database'
+    roundCount = 'round'
+    if questionParam in kwargs and answerParam in kwargs and kwargs[questionParam] is not None and kwargs[answerParam] is not None:
+        question = kwargs[questionParam]
+        answers = kwargs[answerParam]
+    else:
+        # Default - we want a database of multiple questions to draw from. And we wnat to randomly select from that database
+        if randomParam in kwargs and kwargs[randomParam] is not True:
+            if databaseParam in kwargs and kwargs[databaseParam] is not None and kwargs[databaseParam] is not "":
+                import importlib
+                mod = importlib.import_module(kwargs[databaseParam])
+                db = mod.db
+            else:
+                from DataBase import db
+            question = list(db.keys())[int(kwargs[roundCount]) - 1] # start from index 0
+            answers = db[question]
+        else:
+            import random
+            if databaseParam in kwargs and kwargs[databaseParam] is not None and kwargs[databaseParam] is not "":
+                import importlib
+                mod = importlib.import_module(kwargs[databaseParam])
+                db = mod.db
+            else:
+                from DataBase import db
+            question = random.choice(list(db.keys()))
+            # Avoid the same question being randomly selected in multiple rounds
+            if usedQuestionsParam in kwargs and kwargs[usedQuestionsParam] is not None:
+                while question in kwargs[usedQuestionsParam]:
+                    question = random.choice(list(db.keys()))
+            answers = db[question]
+    return (question, answers)
 
 def main(**kwargs):
-    questionParam = 'question'
-    answerParam = 'answers'
     playerParam = 'contestants'
-    if questionParam in kwargs and answerParam in kwargs and kwargs[questionParam] is not None and kwargs[answerParam] is not None:
-        g_question = kwargs[questionParam]
-        g_answers = kwargs[answerParam]
-    else:
-        g_question = random.choice(list(db.keys()))
-        g_answers = db[g_question]
+    kwargs['round'] = "1"
+    g_question, g_answers = GetQuestionAndAnswers(**kwargs)
     if playerParam in kwargs and kwargs[playerParam] is not None:
         g_contestants = kwargs[playerParam]
     else:
@@ -136,36 +322,27 @@ def main(**kwargs):
     offsets.PlayerEnd = offsets.PlayerStart + len(g_contestants) - 1
     offsets.SubmitButton = offsets.PlayerEnd + 1
     # y offsets (0 == Top, (max) == bototm)
-    offsets.RoundMultiplier = 0
-    offsets.Question = 1
-    offsets.Names = 2
-    offsets.AnswerBegin = 3
+    offsets.Question = 0
+    offsets.Names = offsets.Question + 1
+    offsets.AnswerBegin = offsets.Names + 1
     offsets.AnswerEnd = offsets.AnswerBegin + len(g_answers) - 1
     offsets.PlayerScore = offsets.AnswerEnd + 1
+    offsets.RoundMultiplier = offsets.PlayerScore
 
     master = tk.Tk()
-    PopulateWindow(master=master, offsets=offsets, question=g_question, answers=g_answers, contestants=g_contestants)
-     
-    def DISABLE(button):
-        button["state"] = "disabled"
-    
-    if len(g_contestants) is 2:
-        # Traditional FF! This means we should keep track of incorrect answers :)
-        for i in range(3):
-            but = tk.Button(master, text='Incorrect guess')
-            but['command'] = lambda b=but: DISABLE(b)
-            but.grid(column = offsets.SubmitButton + 1, row=offsets.AnswerBegin + 2*i, rowspan=2)
-
-    but = tk.Button(master, text = 'Next Round')
-    # but['command'] = lambda nextround()
-    but.grid(columnspan=10, row = offsets.PlayerScore + 1)
+    PopulateWindow(
+        master=master,
+        offsets=offsets,
+        question=g_question,
+        answers=g_answers,
+        contestants=g_contestants,
+        round=kwargs['round'])
     tk.mainloop()
 
 if __name__ == "__main__":
     # Argparse and sys are for CLI arguments
     # random and DataBase are for default parameters
-    import argparse, sys, random
-    from DataBase import db
+    import argparse, sys
     if sys.version_info.minor > 7 and sys.version_info.major >= 3:
         act = 'extend'
     else:
@@ -182,6 +359,25 @@ if __name__ == "__main__":
     parser.add_argument('-nr','--newresults',
         action=act,
         help='Specify your own survey responses to the commandline. Only valid if "ns" specified. Should take the form of "(Answer,ResponseCount)"')
+    
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ('yes','true','t','y','1'):
+            return True
+        elif v.lower() in ('no','false','f','n','0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected')
+    
+    parser.add_argument('-r','--random',
+        type = str2bool,
+        nargs = '?',
+        const = True,
+        default = True,
+        help='If set, randomly chooses a prompt from the proided (or default) database file. Else tries to go in order')
+    parser.add_argument('-if','--inputfile',
+        help='Input Database file. Should match the format of database.py')
         
     ns = parser.parse_args(sys.argv[1:])
     params = {}
@@ -200,5 +396,8 @@ if __name__ == "__main__":
     if ns.newsurvey is None or ns.newresults is None:
         params['question'] = ns.newsurvey
         params['answers'] = ns.newresults
-    print("Arguments passed to main: " + str(params))
+    if ns.random is not None:
+        params['random'] = ns.random
+    if ns.inputfile is not None:
+        params['database'] = ns.inputfile
     main(**params)
--- README.md
+++ README.md
@@ -4,9 +4,15 @@ This is most definitely *not* a free open-sourced program that allows one to pla
 # TODO ( No particular order of importance  / implementation)
 *) Arg parsing should allow specific games in the database to be played
 *) Unit tests would be nice
-*) GM Window should allow for a multiplier for rounds
-*) GM Window should allow for multiple rounds w/o being restarted
 *) Game window should .... work
 *) Game window should be tied into GM Window such that all values are reflected and 
     updated in real time
-*) GM Window should allow for a parameter of new Q/A (needs to be tested)
+*) Should scrape more questions for the database
+
+# To test
+*) new q/a from cli params
+*) import db file from cli params
+
+
+# Known bugs
+*) Something with the "next round" button causes an issue if the next round has fewer answers than the previous. Should be easy enough to find & fix
